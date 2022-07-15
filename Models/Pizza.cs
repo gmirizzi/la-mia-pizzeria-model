488267f67ebe5eac0714ea42b952e4a4c4d3a8a8
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-15 12:40:43 +0200

    Creato model pizza

---
+++ Models/Pizza.cs
@@ -0,0 +1,9 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImgPath { get; set; }
+    }
+}

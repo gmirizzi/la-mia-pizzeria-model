d982c482077bc81a380ff943e7aaa93dcb898b07
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-15 14:36:49 +0200

    creato db context

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,8 +1,16 @@
-﻿namespace la_mia_pizzeria_static.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
 {
+    [Table("Pizza")]
     public class Pizza
     {
+        [Key]
+        public int PizzaId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Column(TypeName = "text")]
         public string Description { get; set; }
         public string ImgPath { get; set; }
     }
---
+++ PizzeriaContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using la_mia_pizzeria_static.Models;
+
+public class PizzeriaContext : DbContext
+{
+    public DbSet<Pizza> Pizzas { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=pizze-db;Integrated Security=True");
+    }
+}
